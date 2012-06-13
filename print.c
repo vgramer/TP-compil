d5d6a94a4bedaffcca85035331a07f924ac55bbe
bugounet <bugounet@bugounet-VirtualBox.(none)>
2012-06-13 15:14:19 +0200

    ecriture polonaise

--- print.c
+++ print.c
@@ -23,32 +23,40 @@ void pprintValueVar(VarDeclP decl) {
   } else printf("\n");
 }
 
+/* Affichage prefixe d'une expression binaire */
+void pprintPrefixTree2(TreeP tree, char *op) {
+  printf("(%s", op);
+  pprint(getChild(tree, 0));
+  printf(" ");
+  pprint(getChild(tree, 1));
+  printf(")");
+}
 
 /* Affichage d'une expression binaire */
 void pprintTree2(TreeP tree, char *op) {
-  printf("(");
   pprint(getChild(tree, 0));
   printf("%s", op);
   pprint(getChild(tree, 1));
-  printf(")");
 }
 
 /* Affichage d'un if then else */
 void pprintIf(TreeP tree) {
-  printf("(if ");
+  printf("SI (");
   pprint(getChild(tree, 0));
-  printf(" then ");
+  printf(") ALORS \n");
   pprint(getChild(tree, 1));
-  printf(" else ");
-  pprint(getChild(tree, 2));
-  printf(")");
+  if(tree->nbChildren == 3){
+	printf("\n SINON \n");
+	pprint(getChild(tree, 2));
+  }
+  printf("\nFINDESI");
 }
 
 /* Affichage d'un PUT */
 void pprintPUT(TreeP tree) {
-  printf("(put( ");
+  printf("ECRIRE( ");
   pprint(getChild(tree, 0));
-  printf("))\n");
+  printf(")\n");
 }
 
 /* Affichage d'un GET */
@@ -58,33 +66,34 @@ void pprintGet() {
 
 /* Affichage de la boucle pour */
 void pprintPour(TreeP tree) {
-  printf("(POUR %s ALLANT DE %d A %d FAIRE (\n",getChild(tree, 0)->u.str,
+  printf("POUR %s ALLANT DE %d A %d FAIRE \n",getChild(tree, 0)->u.str,
 												getChild(tree, 1)->u.val,
 												getChild(tree, 2)->u.val);
   pprint(getChild(tree, 3));
-  printf("\n) FIN POUR)");
+  printf("\nFINPOUR");
 }
 /* Affichage de la boucle tantque - faire */
 void pprintTantQue(TreeP tree) {
-  printf("(TANQUE ");
+  printf("TANQUE ");
   pprint(getChild(tree,0));
-  printf(" FAIRE (\n");
+  printf(" FAIRE \n");
   pprint(getChild(tree,1));
-  printf("\n) FINTANTQUE)");
+  printf("\nFINTANTQUE");
 }
 /* Affichage de la boucle faire - tantque */
 void pprintFaireTantQue(TreeP tree) {
-  printf("(FAIRE (\n");
+  printf("FAIRE\n");
   pprint(getChild(tree,0));
-  printf("\n) TANTQUE ");
+  printf("\nTANTQUE ");
   pprint(getChild(tree,1));
-  printf(" FINTANTQUE)");
+  printf("FINTANTQUE");
 }
 /* Affichage d'un operateur unaire (GET, NOT) */
 void pprintUnaire(TreeP tree, char* op) {
-  printf("(%s( ", op);
+  printf("(");
   pprint(getChild(tree, 0));
-  printf("))");
+  printf("%s)", op);
+
 }
 /* Affichage recursif d'un arbre representant une expression. */
 void pprint(TreeP tree) {
@@ -101,15 +110,15 @@ void pprint(TreeP tree) {
   case GE:    pprintTree2(tree, " >= "); break;
   case LT:    pprintTree2(tree, " < "); break;
   case LE:    pprintTree2(tree, " <= "); break;
-  case ADD:   pprintTree2(tree, " + "); break;
-  case SUB:   pprintTree2(tree, " - "); break;
-  case MUL:   pprintTree2(tree, " * "); break;
-  case DIV:   pprintTree2(tree, " / "); break;
-  case AND:   pprintTree2(tree, " and "); break;
-  case OR:    pprintTree2(tree, " or "); break;
-  case BINAND:pprintTree2(tree, " binand "); break;
-  case BINOR: pprintTree2(tree, " binor "); break;
-  case BINXOR:pprintTree2(tree, " xor "); break;
+  case ADD:   pprintPrefixTree2(tree, " + "); break;
+  case SUB:   pprintPrefixTree2(tree, " - "); break;
+  case MUL:   pprintPrefixTree2(tree, " * "); break;
+  case DIV:   pprintPrefixTree2(tree, " / "); break;
+  case AND:   pprintPrefixTree2(tree, " and "); break;
+  case OR:    pprintPrefixTree2(tree, " or "); break;
+  case BINAND:pprintPrefixTree2(tree, " binand "); break;
+  case BINOR: pprintPrefixTree2(tree, " binor "); break;
+  case BINXOR:pprintPrefixTree2(tree, " xor "); break;
   case AFF:   pprintTree2(tree, " := "); break;
   case INSTRL:pprintTree2(tree, " \n "); break; 
   case ARGL:  pprintTree2(tree, ", "); break;
